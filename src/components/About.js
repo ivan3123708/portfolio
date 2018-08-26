cc0efa9c73386ae826e453662b6c78b4e51d2196
ivan3123708 <ivan3123708@gmail.com>
2018-08-26 03:51:43 +0200

    Added skills, edited about

--- src/components/About.js
+++ src/components/About.js
@@ -8,8 +8,8 @@ const About = () => (
         <img src={require('../img/az.jpg')} alt="me"/>
       </div>
       <div className="text">
-        <p>Hi, my name is Ivan and I'm front-end developer from Serbia. I started coding in highschool and since then it became my hobby. Before turning it into profession I used to play around with making and designing websites. Over the last couple of years I'm more oriented towards developing web applications using JavaScript and its libraries, especially React.</p>
-        <p>Together with constantly expanding my knowledge of React ecosystem and other front-end technologies, I enjoy learning new skills such as Node and Express in order to get into backend and be able to build more powerful applications. I also love solving coding problems on <a href="https://www.codewars.com/users/ivan3123708" target="blank">Codewars</a>.</p>
+        <p>Hi, my name is Ivan and I'm front-end developer from Serbia. I started coding in high school and since then it became my hobby. Before turning it into profession I used to play around with making and designing websites.</p>
+        <p>Now I'm developing fullstack applications, mostly using React, Node and MongoDB (MERN stack). I tend to write clean and reusable, strongly typed code, easy to read and test. Always keep up with the newest technologies and best practices. Constantly learning new things, contributing to open source projects, and also love solving coding problems on <a href="https://www.codewars.com/users/ivan3123708" target="blank">Codewars</a>.</p>
       </div>
     </div>
     <div className="skills">
@@ -17,6 +17,7 @@ const About = () => (
       <img src={require('../img/html5.png')} alt="html5" />
       <img src={require('../img/css3.png')} alt="css3" />
       <img src={require('../img/javascript.png')} alt="javascript" />
+      <img src={require('../img/typescript.png')} alt="typescript" />
       <img src={require('../img/jquery.png')} alt="jquery" />
       <img src={require('../img/scss.png')} alt="scss" />
       <img src={require('../img/react.png')} alt="react" />
@@ -26,6 +27,7 @@ const About = () => (
       <img src={require('../img/mongodb.png')} alt="mongodb" />
       <img src={require('../img/material-ui.png')} alt="material-ui" />
       <img src={require('../img/webpack.png')} alt="webpack" />
+      <img src={require('../img/jest.png')} alt="jest" />
       <img src={require('../img/git.png')} alt="git" />
       <img src={require('../img/linux.png')} alt="linux" />
     </div>
